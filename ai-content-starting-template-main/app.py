258d1f868e78a9b43e2f0d1b83f83a65718511f7
Jan <janeksus21@gmail.com>
2023-03-08 01:34:16 +0100

    small changes and experiments

--- ai-content-starting-template-main/app.py
+++ ai-content-starting-template-main/app.py
@@ -5,7 +5,6 @@ import aicontent
 def page_not_found(e):
   return render_template('404.html'), 404
 
-
 app = Flask(__name__)
 app.config.from_object(config.config['development'])
 app.register_error_handler(404, page_not_found)
